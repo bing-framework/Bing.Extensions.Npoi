d12a5059148a72eecfb7382f16d2626ff43b61c7
jian玄冰 <jianxuanhuo1@126.com>
2024-06-04 14:22:44 +0800

    ✨ feat(IWorkbook): 增加 扩展方法

---
+++ src/Bing.Extensions.Npoi/Bing/Npoi/SheetExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using NPOI.SS.UserModel;
+
+namespace Bing.Npoi;
+
+/// <summary>
+/// 工作表(<see cref="ISheet"/>) 扩展
+/// </summary>
+public static class SheetExtensions
+{
+    /// <summary>
+    /// 获取或创建指定位置的单元格。
+    /// </summary>
+    /// <param name="sheet">工作表对象。</param>
+    /// <param name="row">行号，从1开始。</param>
+    /// <param name="col">列号，从1开始。</param>
+    /// <returns>指定位置的单元格。</returns>
+    public static ICell Cell(this ISheet sheet, int row, int col)
+    {
+        var targetRow = sheet.GetRow(row - 1) ?? sheet.CreateRow(row - 1);
+        return targetRow.GetCell(col - 1) ?? targetRow.CreateCell(col - 1);
+    }
+
+    /// <summary>
+    /// 获取指定位置单元格的值。
+    /// </summary>
+    /// <param name="sheet">工作表对象。</param>
+    /// <param name="row">行号，从1开始。</param>
+    /// <param name="col">列号，从1开始。</param>
+    /// <returns>指定位置单元格的值，返回类型为动态类型。</returns>
+    public static dynamic Get(this ISheet sheet, int row, int col)
+    {
+        var cell = sheet.Cell(row, col);
+        switch (cell.CellType)
+        {
+            case CellType.Numeric:
+                return cell.NumericCellValue;
+            case CellType.String:
+                return cell.StringCellValue;
+            case CellType.Boolean:
+                return cell.BooleanCellValue;
+            case CellType.Error:
+                return cell.ErrorCellValue;
+            case CellType.Formula:
+                return cell.CellFormula;
+            default:
+                return "null";
+        }
+    }
+
+    public static void Set(this ISheet sheet, int row, int col, object value)
+    {
+        var cell = sheet.Cell(row, col);
+        switch (value)
+        {
+            case null:
+                cell.SetCellValue("");
+                break;
+            case string s:
+                cell.SetCellValue(s);
+                break;
+            case double:
+            case int:
+            case float:
+            case long:
+            case decimal:
+                cell.SetCellValue(Convert.ToDouble(value));
+                break;
+            case DateTime time:
+                cell.SetCellValue(time);
+                break;
+            case bool b:
+                cell.SetCellValue(b);
+                break;
+            default:
+                cell.SetCellValue(value.ToString());
+                break;
+        }
+    }
+}
--- src/Bing.Extensions.Npoi/Bing/Npoi/WorkbookExtensions.cs
+++ src/Bing.Extensions.Npoi/Bing/Npoi/WorkbookExtensions.cs
@@ -1,26 +1,115 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using NPOI.SS.Formula;
 using NPOI.SS.UserModel;
 
-namespace Bing.Npoi
+namespace Bing.Npoi;
+
+/// <summary>
+/// NPOI工作簿(<see cref="IWorkbook"/>) 扩展
+/// </summary>
+public static partial class WorkbookExtensions
 {
     /// <summary>
-    /// NPOI工作簿(<see cref="IWorkbook"/>) 扩展
+    /// 将工作簿转换为字节数组。
     /// </summary>
-    public static partial class WorkbookExtensions
+    /// <param name="workbook">要转换的工作簿。</param>
+    /// <param name="leaveOpen">是否保持工作簿打开状态，默认为false。</param>
+    /// <returns>表示工作簿的字节数组。</returns>
+    /// <exception cref="ArgumentNullException">如果工作簿为null，则抛出此异常。</exception>
+    public static byte[] ToBytes(this IWorkbook workbook, bool leaveOpen = false)
     {
-        /// <summary>
-        /// 将工作簿保存到二进制文件流
-        /// </summary>
-        /// <param name="workbook">NPOI工作簿</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static byte[] SaveToBuffer(this IWorkbook workbook)
+        if (workbook is null)
+            throw new ArgumentNullException(nameof(workbook));
+        using var stream = new MemoryStream();
+        workbook.Write(stream, leaveOpen);
+        return stream.ToArray();
+    }
+
+    /// <summary>
+    /// 将工作簿写入指定的文件中。
+    /// </summary>
+    /// <param name="workbook">要写入的工作簿。</param>
+    /// <param name="fileName">目标文件名。</param>
+    /// <param name="leaveOpen">是否保持工作簿打开状态，默认为false。</param>
+    /// <exception cref="ArgumentNullException">如果工作簿为null，则抛出此异常。</exception>
+    public static void ToFile(this IWorkbook workbook, string fileName, bool leaveOpen = false)
+    {
+        if (workbook is null)
+            throw new ArgumentNullException(nameof(workbook));
+        using var fs = new FileStream(fileName, FileMode.Create);
+        workbook.Write(fs, leaveOpen);
+    }
+
+    /// <summary>
+    /// 计算工作簿中所有公式单元格的值。
+    /// </summary>
+    /// <param name="workbook">要计算的工作簿。</param>
+    /// <exception cref="ArgumentNullException">如果工作簿为null，则抛出此异常。</exception>
+    public static void EvaluateAllFormulaCells(this IWorkbook workbook)
+    {
+        if (workbook is null)
+            throw new ArgumentNullException(nameof(workbook));
+        BaseFormulaEvaluator.EvaluateAllFormulaCells(workbook);
+    }
+
+    /// <summary>
+    /// 计算工作簿中所有公式单元格的值，忽略任何错误。
+    /// </summary>
+    /// <param name="workbook">要计算的工作簿。</param>
+    /// <exception cref="ArgumentNullException">如果工作簿为null，则抛出此异常。</exception>
+    public static void EvaluateAllFormulaCellsIgnoreError(this IWorkbook workbook)
+    {
+        if (workbook is null)
+            throw new ArgumentNullException(nameof(workbook));
+        var evaluator = workbook.GetCreationHelper().CreateFormulaEvaluator();
+        for (var i = 0; i < workbook.NumberOfSheets; i++)
         {
-            if (workbook is null)
-                throw new ArgumentNullException(nameof(workbook));
-            using var ms = new MemoryStream();
-            workbook.Write(ms);
-            return ms.ToArray();
+            foreach (IRow item in workbook.GetSheetAt(i))
+            {
+                foreach (var cell in item)
+                {
+                    if (cell.CellType == CellType.Formula)
+                    {
+                        try
+                        {
+                            evaluator.EvaluateFormulaCell(cell);
+                        }
+                        catch
+                        {
+                            // 不做处理，为了后续执行后续单元格
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    /// <summary>
+    /// 创建一个公式求值器，用于计算工作簿中的公式单元格。
+    /// </summary>
+    /// <param name="workbook">要创建公式求值器的工作簿。</param>
+    /// <returns>创建的公式求值器。</returns>
+    /// <exception cref="ArgumentNullException">如果工作簿为null，则抛出此异常。</exception>
+    public static IFormulaEvaluator CreateFormulaEvaluator(this IWorkbook workbook)
+    {
+        if (workbook is null)
+            throw new ArgumentNullException(nameof(workbook));
+        return workbook.GetCreationHelper().CreateFormulaEvaluator();
+    }
+
+    /// <summary>
+    /// 从工作簿中获取第一个存在的工作表。
+    /// </summary>
+    /// <param name="workbook">要获取工作表的工作簿。</param>
+    /// <param name="names">工作表名称列表，按照顺序查找，返回第一个存在的工作表。</param>
+    /// <returns>找到的第一个工作表，如果没有找到则返回null。</returns>
+    /// <exception cref="ArgumentNullException">如果工作簿为null，则抛出此异常。</exception>
+    public static ISheet GetSheet(this IWorkbook workbook, params string[] names)
+    {
+        if (workbook is null)
+            throw new ArgumentNullException(nameof(workbook));
+        return names.Select(workbook.GetSheet).FirstOrDefault(sheet => sheet != null);
     }
 }
