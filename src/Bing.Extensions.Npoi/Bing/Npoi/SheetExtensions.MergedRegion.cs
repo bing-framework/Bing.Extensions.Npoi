8f1a1de0fd5a4746189266f28c38fe4e07edbf8b
jian玄冰 <jianxuanhuo1@126.com>
2024-06-11 16:02:56 +0800

    🐛 fix: 调整 扩展方法

---
+++ src/Bing.Extensions.Npoi/Bing/Npoi/SheetExtensions.MergedRegion.cs
@@ -0,0 +1,8 @@
+﻿using NPOI.SS.UserModel;
+
+namespace Bing.Npoi;
+
+// 工作表扩展 - 合并区域
+public static partial class SheetExtensions
+{
+}
--- src/Bing.Extensions.Npoi/Bing/Npoi/SheetExtensions.cs
+++ src/Bing.Extensions.Npoi/Bing/Npoi/SheetExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NPOI.SS.UserModel;
 
 namespace Bing.Npoi;
@@ -6,7 +7,7 @@ namespace Bing.Npoi;
 /// <summary>
 /// 工作表(<see cref="ISheet"/>) 扩展
 /// </summary>
-public static class SheetExtensions
+public static partial class SheetExtensions
 {
     /// <summary>
     /// 获取或创建指定位置的单元格。
@@ -40,6 +41,38 @@ public static class SheetExtensions
     }
 
     /// <summary>
+    /// 在指定的行索引位置插入一行新行。
+    /// </summary>
+    /// <param name="sheet">要插入新行的工作表。</param>
+    /// <param name="rowIndex">新行的行索引。</param>
+    /// <returns>插入的新行。</returns>
+    /// <exception cref="ArgumentNullException">如果工作表为null，则抛出此异常。</exception>
+    public static IRow InsertRow(this ISheet sheet, int rowIndex) => sheet.InsertRows(rowIndex, 1)[0];
+
+    /// <summary>
+    /// 在指定的行索引位置插入指定数量的新行。
+    /// </summary>
+    /// <param name="sheet">要插入新行的工作表。</param>
+    /// <param name="rowIndex">新行的起始行索引。</param>
+    /// <param name="rowsCount">要插入的新行的数量。</param>
+    /// <returns>插入的新行数组。</returns>
+    /// <exception cref="ArgumentNullException">如果工作表为null，则抛出此异常。</exception>
+    public static IRow[] InsertRows(this ISheet sheet, int rowIndex, int rowsCount)
+    {
+        if (sheet == null)
+            throw new ArgumentNullException(nameof(sheet));
+        if (rowIndex <= sheet.LastRowNum)
+            sheet.ShiftRows(rowIndex, sheet.LastRowNum, rowsCount, true, false);
+        var rows = new List<IRow>();
+        for (var i = 0; i < rowsCount; i++)
+        {
+            var row = sheet.CreateRow(rowIndex + i);
+            rows.Add(row);
+        }
+        return rows.ToArray();
+    }
+
+    /// <summary>
     /// 获取指定位置单元格的值。
     /// </summary>
     /// <param name="sheet">要获取值的工作表。</param>
@@ -64,37 +97,40 @@ public static class SheetExtensions
     }
 
     /// <summary>
-    /// 获取公式单元格的计算结果值。
+    /// 检查目标范围是否在指定范围内部或与其相交。
     /// </summary>
-    /// <param name="cell">要获取值的单元格。</param>
-    /// <returns>公式单元格的计算结果值。返回值类型根据公式的计算结果类型而定。如果单元格不是公式类型，或者公式尚未计算，返回空字符串。</returns>
-    public static void Set(this ISheet sheet, int row, int col, object value)
+    /// <param name="rangeMinRow">指定范围的最小行索引。</param>
+    /// <param name="rangeMaxRow">指定范围的最大行索引。</param>
+    /// <param name="rangeMinCol">指定范围的最小列索引。</param>
+    /// <param name="rangeMaxCol">指定范围的最大列索引。</param>
+    /// <param name="targetMinRow">目标范围的最小行索引。</param>
+    /// <param name="targetMaxRow">目标范围的最大行索引。</param>
+    /// <param name="targetMinCol">目标范围的最小列索引。</param>
+    /// <param name="targetMaxCol">目标范围的最大列索引。</param>
+    /// <param name="onlyInternal">如果为true，只检查目标范围是否在指定范围内部；如果为false，检查目标范围是否在指定范围内部或与其相交。</param>
+    /// <returns>如果目标范围在指定范围内部或与其相交，返回true；否则返回false。</returns>
+    private static bool IsInternalOrIntersect(
+        int? rangeMinRow, int? rangeMaxRow, 
+        int? rangeMinCol, int? rangeMaxCol, 
+        int targetMinRow, int targetMaxRow, 
+        int targetMinCol, int targetMaxCol, 
+        bool onlyInternal)
     {
-        var cell = sheet.GetOrCreateCell(row, col);
-        switch (value)
+        var tempMinRow = rangeMinRow ?? targetMinRow;
+        var tempMaxRow = rangeMaxRow ?? targetMaxRow;
+        var tempMinCol = rangeMinCol ?? targetMinCol;
+        var tempMaxCol = rangeMaxCol ?? targetMaxCol;
+        if (onlyInternal)
         {
-            case null:
-                cell.SetCellValue("");
-                break;
-            case string s:
-                cell.SetCellValue(s);
-                break;
-            case double:
-            case int:
-            case float:
-            case long:
-            case decimal:
-                cell.SetCellValue(Convert.ToDouble(value));
-                break;
-            case DateTime time:
-                cell.SetCellValue(time);
-                break;
-            case bool b:
-                cell.SetCellValue(b);
-                break;
-            default:
-                cell.SetCellValue(value.ToString());
-                break;
+            return tempMinRow <= targetMinRow &&
+                   tempMaxRow >= targetMaxRow &&
+                   tempMinCol <= targetMinCol &&
+                   tempMaxCol >= targetMaxCol;
         }
+
+        return Math.Abs(tempMaxRow - tempMinRow) + Math.Abs(targetMaxRow - targetMinRow) >=
+               Math.Abs(tempMaxRow + tempMinRow - targetMaxRow - targetMinRow) &&
+               Math.Abs(tempMaxCol - tempMinCol) + Math.Abs(targetMaxCol - targetMinCol) >=
+               Math.Abs(tempMaxCol + tempMinCol - targetMaxCol - targetMinCol);
     }
 }
